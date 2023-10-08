7421e213a91cbd2e94c1b334f792b380e6b270e9
unknown <darknemiroff@gmail.com>
2023-10-08 23:47:36 +0400

    Add settings and menu open box

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,12 @@
+let menuBox = document.querySelector('.menu_box')
+let isMenuOpen = false
 function menuFn() {
-    console.log('menu');
+    isMenuOpen = !isMenuOpen
+    if(isMenuOpen === true){
+        isSettingOpen = false
+        menuBox.style.display = 'block'
+        settingsBox.style.display = 'none'
+    }else {
+        menuBox.style.display = 'none'
+    }
 }
--- js/settings.js
+++ js/settings.js
@@ -1,3 +1,13 @@
+let settingsBox = document.querySelector('.settings_box')
+let isSettingOpen = false
 function settingFn(){
     console.log('hello');
+    isSettingOpen = !isSettingOpen
+    if(isSettingOpen === true){
+        isMenuOpen = false
+        settingsBox.style.display = 'block'
+        menuBox.style.display = 'none'
+    }else {
+        settingsBox.style.display = 'none'
+    }
 }
